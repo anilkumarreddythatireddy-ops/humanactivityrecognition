54e1b7efaea5595a76747ab2c715e4bae513c399
Anil kumar reddy <anilkumarreddy.thatireddy@gmail.com>
2026-08-16 19:04:47 +0530

    Remove old checkpoint files

--- checkpoints/check_mpii.py
+++
@@ -1,8 +0,0 @@
-from preprocessing.mpii_dataset import MPIIDataset
-
-dataset = MPIIDataset(
-    image_dir="datasets/mpii/images",
-    annotation_file="datasets/mpii/annotations/mpii_human_pose_v1_u12_1.mat"
-)
-
-print(len(dataset))
